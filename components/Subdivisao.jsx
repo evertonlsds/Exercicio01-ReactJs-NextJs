bb9373d05cd96adf0f64bb70a861d7fd3c364fa5
Everton santos <everton0102@gmail.com>
2021-11-26 18:03:10 -0300

    criação de componentes

--- components/Subdivisao.jsx
+++ components/Subdivisao.jsx
@@ -1,8 +1,16 @@
 import styles from '../styles/Subdivisao.module.css'
 
 
+
 export default function Subdivisao(props){
     return(
-        <div className={styles.Subdivisao}></div>
+        <div 
+            style= {{
+                backgroundColor: props.preta ? "#000" : "#fff"
+            }}      
+            className= {styles.subdivisao}>
+               
+
+        </div>
     )
 }
